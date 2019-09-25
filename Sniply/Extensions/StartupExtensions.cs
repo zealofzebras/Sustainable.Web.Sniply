7d8e8b632887b912a8b25be54f93b80ea5e65ba2
Peter <peter@zeal.global>
2019-09-25 15:12:56 +0200

    Added extensions methods for easy setup and configuration

---
+++ Sniply/Extensions/StartupExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sniply.Extensions
+{
+    public static class StartupExtensions
+    {
+
+        public static IServiceCollection AddSniply(this IServiceCollection services, Action<SniplyOptions> options)
+        {
+            return services.Configure(options)
+                           .AddSingleton<SniplyReplacer>();
+
+        }
+
+        public static PageConventionCollection SetSniplyPath(this PageConventionCollection conventions, string sniplyPath)
+        {
+            return conventions.AddAreaPageRoute("Sniply", "/Index", $"{sniplyPath}/{{*url}}");
+        }
+    }
+}
